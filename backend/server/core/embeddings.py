72f8f4276297f56e4b19e94b2230ad51924d58ed
aban-m <aban.salah.mahmoud@gmail.com>
2024-12-20 13:39:46 +0200

    Added debugging helper

--- backend/server/core/embeddings.py
+++ backend/server/core/embeddings.py
@@ -4,7 +4,7 @@ import os
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
 def embed(text):
-    return requests.post('https://api.openai.com/v1/embeddings',
+    resp = requests.post('https://api.openai.com/v1/embeddings',
         json = {
             'model': 'text-embedding-3-small',
             'dimensions': 256,
@@ -13,4 +13,6 @@ def embed(text):
         headers = {
             'Authorization': f'Bearer {OPENAI_API_KEY}'
         }
-    ).json()['data'][0]['embedding']
+    )
+    if not resp.ok(): print(resp); print(resp.data)
+    return resp.json()['data'][0]['embedding']
