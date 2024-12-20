45a3d2f1aca958cad475d8d4665e23372c960d23
aban-m <aban.salah.mahmoud@gmail.com>
2024-12-20 09:31:33 +0200

    Added initial Deployment workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,35 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        changeOrigin: true,
-        secure: false,
-        target: 'http://127.0.0.1:5000'
-      }
-    }
+export default defineConfig(({ command, mode }) => {
+  console.log(`Mode: ${command}`); 
+
+  if (command === 'build') {
+    return {
+      plugins: [react()],
+      base: '/Scatterbrain/', 
+      server: {
+        proxy: {
+          '/api': {
+            changeOrigin: true,
+            secure: false,
+            target: 'https://abanm.pythonanywhere.com/', // Production backend
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      plugins: [react()],
+      server: {
+        proxy: {
+          '/api': {
+            changeOrigin: true,
+            secure: false,
+            target: 'http://localhost:5000/', // Local development backend
+          },
+        },
+      },
+    };
   }
-})
+});
