974dbbb8cab1cf4ddebe217356d7498f89e7cbb7
aban-m <aban.salah.mahmoud@gmail.com>
2024-12-22 09:55:10 +0200

    Refactoring utils-apiviews

--- backend/server/utils.py
+++ backend/server/utils.py
@@ -1,12 +1,6 @@
 from server.core.pca import pca
 from server.db import ops
 
-def jsonify_entries(conn, user_id):
-    entries = [dict(x) for x in ops.read_user(conn, user_id)]
-    for entry in entries:
-        entry['url'] = url_for('api.image', user_id, eid = entry['entry_id'])\
-                       if entry['is_image'] else ''
-    return {'entries': entries}
 
 def sync_pcas(conn, user_id):
     emb_matrix = ops.read_matrix(conn, user_id, pca = False)
