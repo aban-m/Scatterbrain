d99f7099d2ae36718fd240e9ce8d1b3bfdb3cfb8
aban-m <aban.salah.mahmoud@gmail.com>
2024-12-24 11:27:17 +0200

    Added over-engineered environment management infrastructure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const baseConfig = {
+   envDir: './envs'
+}
+
 export default defineConfig(({ command, mode }) => {
   console.log(`Mode: ${command}`); 
 
   if (command === 'build') {
     return {
+      ...baseConfig,
       plugins: [react()],
       base: '/Scatterbrain/'
     };
   } else {
     return {
+      ...baseConfig,
       plugins: [react()]
     };
   }
